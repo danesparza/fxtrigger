e3f7695c7db1b43c67fd5355d015106c244d4e23
Dan Esparza <esparza.dan@gmail.com>
2021-05-24 22:39:27 -0400

    Add standard version handler

--- cmd/version.go
+++ cmd/version.go
@@ -21,31 +21,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// BuildVersion is the app version number at build time
+var BuildVersion = "Unknown"
+
+// CommitID is the SHA commit for the compiled app at build time
+var CommitID string
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Shows the version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		//	Show the version number
+		fmt.Printf("\nfxaudio version %s", BuildVersion)
+
+		//	Show the CommitID if available:
+		if CommitID != "" {
+			fmt.Printf(" (%s)", CommitID[:7])
+		}
+
+		//	Trailing space and newline
+		fmt.Println(" ")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
