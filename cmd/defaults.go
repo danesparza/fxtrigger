7e8565bf227818c1c533dc4f3a4e2695668d9a43
Dan Esparza <esparza.dan@gmail.com>
2021-05-24 22:41:12 -0400

    Add standard defaults handler.  Update version handler output

--- cmd/defaults.go
+++ cmd/defaults.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -21,31 +6,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	jsonConfig bool
+	yamlConfig bool
+)
+
+var yamlDefault = []byte(`
+loglevel: INFO
+`)
+
+var jsonDefault = []byte(`{	
+	"loglevel": "INFO"
+}`)
+
 // defaultsCmd represents the defaults command
 var defaultsCmd = &cobra.Command{
 	Use:   "defaults",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints default fxtrigger configuration file",
+	Long: `Use this to create a default configuration file for fxtrigger. 
+	Example:
+	fxtrigger defaults > fxtrigger.yaml`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("defaults called")
+		if jsonConfig {
+			fmt.Printf("%s", jsonDefault)
+		} else if yamlConfig {
+			fmt.Printf("%s", yamlDefault)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(defaultsCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// defaultsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// defaultsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	defaultsCmd.Flags().BoolVarP(&jsonConfig, "json", "j", false, "Create a JSON configuration file")
+	defaultsCmd.Flags().BoolVarP(&yamlConfig, "yaml", "y", true, "Create a YAML configuration file")
 }
